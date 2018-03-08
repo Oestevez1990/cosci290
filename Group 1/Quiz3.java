f86248cc001a494016e1fbf35540a797a075b009
heyitsmeomar <heyitsmeomar@aol.com>
2018-03-08 17:37:15 -0500

    Group1 Quiz 3

---
+++ Group 1/Quiz3.java
@@ -0,0 +1,34 @@
+/*
+Omar Estevez
+CoSci 290
+3/8/18
+Group 1 Quiz 3
+
+*/
+
+public class Quiz3{
+  
+  public static void main(String [] args){
+    
+    System.out.println("a      a^2     a^3");
+      
+    
+    System.out.println((1 + "\t" +  1*1 + "\t" + 1*1*1));
+      
+    
+    
+    System.out.println((2 + "\t" +  2*2 + "\t" + 2*2*2));
+      
+    
+    
+    System.out.println((3 + "\t"  + 3*3 + "\t" + 3*3*3));
+      
+    
+    
+    System.out.println((4 + "\t"  + 4*4 + "\t" + 4*4*4));
+      
+    
+     
+      
+  }
+}
